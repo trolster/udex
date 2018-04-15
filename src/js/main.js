804800a36d57b7c8bee6e201facf83d3304f9594
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-04-15 22:43:08 +0200

    add some buttons to pages when using the extension

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  state = {
+    show: false
+  }
+  handleShowAll() {
+    this.setState({show: !this.state.show})
+  }
+  handleClick() {
+    this.setState({show: !this.state.show})
+    console.log('please work')
+  }
   render() {
     return (
-      <div> Your App injected to DOM correctly! </div>
+      <div>
+        <button onClick={() => this.handleClick()}>Click Me!</button>
+        {this.state.show && 
+          <input
+            type="button" 
+            value="another button to click!"
+            onClick={() => this.handleShowAll()}
+          />
+        }
+      </div>
     )
   }
 }
@@ -12,7 +31,7 @@ class App extends React.Component {
 // Message Listener function
 chrome.runtime.onMessage.addListener((request, sender, response) => {
   // If message is injectApp
-  if (request.injectApp) {
+  if (request.injectApp) {  
     // Inject our app to DOM and send response
     injectApp();
     response({
@@ -24,6 +43,6 @@ chrome.runtime.onMessage.addListener((request, sender, response) => {
 function injectApp() {
   const newDiv = document.createElement("div");
   newDiv.setAttribute("id", "udex");
-  document.body.appendChild(newDiv);
+  document.body.prepend(newDiv);
   ReactDOM.render(<App />, newDiv);
 }
