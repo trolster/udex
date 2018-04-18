dbc76da9dbdd12825fdc7e7d2402a761702501f3
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-04-18 14:54:42 +0200

    fix onClick handlers for buttons

--- src/js/components/Buttons.js
+++ src/js/components/Buttons.js
@@ -7,6 +7,25 @@ const Button = styled.button`
   border-radius: 5px;
 `;
 
-export const OpenAll = () => <Button>Open All</Button>;
+export class OpenAll extends React.Component {
+  handleClick() {
+    const radioButtons = document.querySelectorAll('input[value="passed"]');
+    for (let i = 0; i < radioButtons.length; i++) {
+      radioButtons[i].click();
+    }
+    console.log(radioButtons);
+    console.log("radioButtons");
+  }
+  render() {
+    return <Button onClick={() => this.handleClick()}>Open All</Button>;
+  }
+}
 
-export const SaveAll = () => <Button>Save All</Button>;
+export class SaveAll extends React.Component {
+  handleClick() {
+    console.log("Pretending to save..");
+  }
+  render() {
+    return <Button onClick={() => this.handleClick()}>Save All</Button>;
+  }
+}
--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,11 @@ import ReactDOM from "react-dom";
 import { OpenAll, SaveAll } from "./components/Buttons";
 
 class App extends React.Component {
-  handleClick() {
-    const radioButtons = document.querySelectorAll('input[value="passed"]');
-    for (let i = 0; i < radioButtons.length; i++) {
-      radioButtons[i].click();
-    }
-    console.log(radioButtons);
-  }
   render() {
     return (
       <div>
-        <OpenAll onClick={() => this.handleClick()} />
+        <OpenAll />
+        <SaveAll />
       </div>
     );
   }
