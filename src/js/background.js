898978a9014adcfaad2d5d23b2507312fbc99380
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-04-22 17:17:58 +0200

    update manifest.json

--- src/js/background.js
+++
@@ -1,7 +0,0 @@
-// Listen for the browserAction
-chrome.browserAction.onClicked.addListener(function(tab) {
-  // Inject "main.js" into the current tab
-  chrome.tabs.executeScript(tab.id, {
-    file: "main.js"
-  });
-});
