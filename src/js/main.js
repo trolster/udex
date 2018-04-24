6dcd638bb7318dd3b227bb5c0d418217e4d9522c
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-04-25 00:00:11 +0200

    add functionality to allow websites to detect if it's installed

--- src/js/main.js
+++ src/js/main.js
@@ -13,14 +13,27 @@ class App extends React.Component {
 }
 
 function injectApp() {
+  // Inject data to let a website know it's installed. The website will have to
+  // be aware of the extension, and include an element with the extension id as
+  // the class name, and a data-is-installed attribute set to false.
+  const marker = document.querySelector(".papbfgdefjaddphbaekhljchpbigedei");
+  if (marker) {
+    // Click on the marker so that the application can detect that the
+    // extension is installed.
+    marker.click();
+  }
+  // Add buttons to the feedback tab.
   const selector = `section[ng-show="currentTab == 'feedback'"]`;
   const feedbackSection = document.querySelector(selector);
-  const top = document.createElement("div");
-  const bottom = document.createElement("div");
-  feedbackSection.prepend(top);
-  feedbackSection.append(bottom);
-  ReactDOM.render(<App />, top);
-  ReactDOM.render(<App />, bottom);
+  if (feedbackSection) {
+    const top = document.createElement("div");
+    const bottom = document.createElement("div");
+    feedbackSection.prepend(top);
+    feedbackSection.append(bottom);
+
+    ReactDOM.render(<App />, top);
+    ReactDOM.render(<App />, bottom);
+  }
 }
 
 injectApp();
