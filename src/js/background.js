e297d227df1008fa48ff6a4ffd4467102b39bc1e
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-05-08 14:16:16 +0200

    create background requestloop to check for assigned and feedbacks

--- src/js/background.js
+++ src/js/background.js
@@ -1,6 +1,35 @@
+const api = {
+  rootUrl: "https://review-api.udacity.com/api/v1/",
+  assignedUrl: "me/submissions/assigned/",
+  feedbacksUrl: "me/student_feedbacks/",
+  headers: { Authorization: "" },
+  interval: 60000 // One minute in ms
+};
+
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.browserAction.setBadgeText({ text: "2" });
-  // chrome.storage.sync.set({ token: "asdf" }, () => {
-  //   console.log("Token initialised.");
-  // });
+  chrome.storage.sync.get("token", function(data) {
+    api.headers.Authorization = data.token;
+    requestLoop();
+  });
 });
+
+const apiCall = async path => {
+  const res = await fetch(`${api.rootUrl}${path}`, {
+    headers: api.headers
+  });
+  return res.json();
+};
+
+const requestLoop = () => {
+  setInterval(async () => {
+    const assigned = await apiCall(api.assignedUrl);
+    const feedbacks = await apiCall(api.feedbacksUrl);
+    const assignedCount = assigned.length.toString();
+    const feedbacksCount = feedbacks.filter(fb => fb.read_at === null).length;
+    const text = `${assignedCount}:${
+      feedbacksCount > 9 ? "9+" : feedbacksCount
+    }`;
+    chrome.browserAction.setBadgeText({ text });
+    console.log("loop called");
+  }, api.interval);
+};
