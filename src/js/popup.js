b5cbbb4e9d5630c5cda9df58c33a65ab825fa813
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-05-08 17:21:45 +0200

    add start/stop button

--- src/js/popup.js
+++ src/js/popup.js
@@ -1,5 +1,6 @@
 const changeToken = document.getElementById("changeToken");
 const tokenInputField = document.getElementById("token");
+const startButton = document.getElementById("start");
 
 const setTokenText = () => {
   chrome.storage.sync.get("token", function(data) {
@@ -8,6 +9,19 @@ const setTokenText = () => {
     tokenInputField.select();
   });
 };
+
+const setStartButtonText = () => {
+  chrome.storage.sync.get("started", data => {
+    startButton.value = data.started === "started" ? "running..." : "start";
+  });
+};
+
+startButton.addEventListener("click", e => {
+  const buttonState = startButton.value === "start" ? "running" : "start";
+  chrome.storage.sync.set({ started: buttonState }, data => {
+    startButton.value = buttonState;
+  });
+});
 
 changeToken.addEventListener("submit", e => {
   e.preventDefault();
