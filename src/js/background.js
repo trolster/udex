a284a569b9a53c32197d0c3966d95ad165b0554e
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-05-08 12:09:21 +0200

    switch to browserAction and add badge text

--- src/js/background.js
+++ src/js/background.js
@@ -1,17 +1,6 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.browserAction.setBadgeText({ text: "2" });
   // chrome.storage.sync.set({ token: "asdf" }, () => {
   //   console.log("Token initialised.");
   // });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([
-      {
-        conditions: [
-          new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { hostEquals: "mtl.now.sh" }
-          })
-        ],
-        actions: [new chrome.declarativeContent.ShowPageAction()]
-      }
-    ]);
-  });
 });
