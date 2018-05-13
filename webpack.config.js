f8c7e0d16b835cbe601dffd1de1db58d48814b1f
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-05-13 02:14:18 +0200

    refactor: add babel-loader and bundling of react code

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,26 @@
 const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 module.exports = {
-  mode: "production",
-  entry: { popup: "./src/popup.js", background: "./src/background.js" },
+  mode: "development",
+  devtool: "inline-source-map", // sourcemaps are removed on packaging
+  entry: {
+    popup: "./src/popup.js",
+    background: "./src/background.js",
+    contentScript: "./src/contentScript.js"
+  },
+  plugins: [new CleanWebpackPlugin(["dist"])],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
   output: {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist")
