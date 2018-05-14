cd431fcd9134158bb10b0ce923b54a16fb8cca50
Mikkel Trolle Larsen <trolster2@gmail.com>
2018-05-14 19:13:31 +0200

    add: styling to popup

--- src/popup.js
+++ src/popup.js
@@ -6,7 +6,8 @@ console = chrome.extension.getBackgroundPage().console;
 class Popup extends React.Component {
   state = {
     running: true,
-    token: ""
+    token: "",
+    valid: true
   };
 
   setRunningState = () => {
@@ -30,17 +31,17 @@ class Popup extends React.Component {
 
   handleTokenChange = e => {
     this.setState({ token: e.target.value });
+    // Validate the token
+    if (e.target.value.length !== 151) {
+      this.setState({ valid: false });
+      return;
+    }
+    this.setState({ valid: true });
   };
 
   handleTokenSubmit = e => {
     e.preventDefault();
     const newToken = this.state.token;
-    // Validate the token
-    if (newToken.length !== 151) {
-      this.setState({ valid: false });
-      return;
-    }
-    this.setState({ valid: true });
 
     // Send a message to background.js when a new token is successfully entered.
     chrome.runtime.sendMessage({ newToken }, res => {
@@ -66,41 +67,81 @@ class Popup extends React.Component {
   render() {
     console.log(this.state);
     const { running, token, valid } = this.state;
-    const labelClass = valid === undefined ? "" : valid ? "valid" : "invalid";
     return (
       <div>
         <input
           type="button"
           id="start-button"
           onClick={this.handleRunningStateChange}
+          className={running ? "running" : ""}
           value={running ? "running" : "start"}
         />
         <form id="change-token" onSubmit={this.handleTokenSubmit}>
-          <label htmlFor="token" className={`token-label ${labelClass}`}>
-            {valid === undefined || valid ? "Current Token" : "Invalid Token"}
+          <label htmlFor="token" className={valid ? "" : "invalid"}>
+            {valid ? "Current Token" : "Invalid Token"}
           </label>
           <textarea
             name="token"
             id="token"
             onFocus={this.handleTokenFocus}
             onChange={this.handleTokenChange}
-            cols="40"
+            cols="50"
             rows="5"
+            placeholder="Your token goes here (must be 151 charactes)..."
             value={token}
           />
           <input type="submit" />
         </form>
         <style jsx>
           {`
+            input[type="button"],
+            input[type="submit"] {
+              background-color: #1f9bcf;
+              border: 0 solid transparent;
+              color: #ffffff;
+              cursor: pointer;
+              display: inline-block;
+              font-size: 0.765625rem;
+              padding: 0.75rem 2rem;
+              text-transform: uppercase;
+              transition: color 0.15s ease-in-out,
+                background-color 0.15s ease-in-out,
+                border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out,
+                -webkit-box-shadow 0.15s ease-in-out;
+              user-select: none;
+            }
+            input:hover {
+              background-color: #1a82ae;
+            }
+            input[type="submit"] {
+              float: right;
+              margin: 10px 0;
+            }
+            input[type="button"] {
+              width: 100%;
+              margin-bottom: 10px;
+            }
+            input[type="button"].running {
+              background-color: #4bbf73;
+            }
+            input[type="button"].running:hover {
+              background-color: #3ca861;
+            }
             input:focus,
-            .token-label {
+            textarea:focus {
               outline: none !important;
             }
-            .valid {
-              background-color: "DarkSeaGreen";
+            label {
+              padding-left: 4px;
+            }
+            textarea {
+              height: 100px;
+              border-color: #1f9bcf;
+              resize: none;
+              padding: 4px;
             }
             .invalid {
-              background-color: "LightCoral";
+              color: #d9534f;
             }
           `}
         </style>
